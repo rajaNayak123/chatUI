49053789ec9344271452e76c8f17826ac7ba9bf5
rajaNayak123 <nayakraja151@gmail.com>
2025-04-14 09:51:49 +0530

    remove unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import ChatApp from "./components/ChatApp.jsx";
 function App() {
